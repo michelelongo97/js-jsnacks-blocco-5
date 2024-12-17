21a34e99c06b1117a967f3e3b20306c2f9404291
michelelongo97 <m.longo7x@gmail.com>
2024-12-17 12:28:28 +0100

    feat: add fourth solution

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,7 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const authorNames = posts.map(post => post.author);
+console.log(authorNames);
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
